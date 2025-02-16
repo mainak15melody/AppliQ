55336c446dbeccec337aa7c45b973a9261051f52
Mainak Das <82697942+mainak15melody@users.noreply.github.com>
2025-02-16 17:55:37 +0530

    Delete mainak.js

--- mainak.js
+++
@@ -1,8 +0,0 @@
-// For extra interactivity, you can add a fading effect for the page content after a few seconds
-
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        const footer = document.querySelector('.footer');
-        footer.style.opacity = '1'; // Make the footer visible after a short delay
-    }, 4000);
-});
